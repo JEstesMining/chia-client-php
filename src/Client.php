f4d9e29d29d8ee0f85aecc59d0728edef0f48d55
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-23 19:12:25 -0400

    updates

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,44 @@
 
 namespace JEstesMining\Component\Chia;
 
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class Client
 {
+    //
+    protected $_client;
+    protected $_options;
+    protected $_dsn;
+
+    /**
+     */
+    public function __construct($dsn, array $options = [])
+    {
+        $this->_dsn     = $dsn;
+        $this->_options = $options;
+    }
+
+    protected function getRpcClient()
+    {
+        if (!$this->_client) {
+            $this->_client = HttpClient::create(array_merge([
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'verify_host' => false,
+                'verify_peer' => false,
+            ], $this->_options));
+        }
+
+        return $this->_client;
+    }
+
+    /**
+     */
+    protected function request(string $method, string $url, array $options = []): ResponseInterface
+    {
+        return $this->getRpcClient()->request($method, $url, $options);
+    }
 }
---
+++ src/Wallet.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace JEstesMining\Component\Chia;
+
+/**
+ * @todo
+ */
+class Wallet extends Client
+{
+}
